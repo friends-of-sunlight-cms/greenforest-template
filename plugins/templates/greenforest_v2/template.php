f345ec09fd5b9c34ea3d8fa88a1edc208ee0c122
laszlobodnarsk <56290156+laszlobodnarsk@users.noreply.github.com>
2023-10-09 23:48:24 +0200

    update nav

--- plugins/templates/greenforest_v2/template.php
+++ plugins/templates/greenforest_v2/template.php
@@ -2,32 +2,18 @@
 use Sunlight\Template;
 defined('SL_ROOT') or exit
 ?>
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-160838059-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'UA-160838059-1');
-</script>
-<style>
-/* width */::-webkit-scrollbar {width: 7px;}
-/* Track */::-webkit-scrollbar-track {background: #ddd;}
-/* Handle */::-webkit-scrollbar-thumb {background: #aaa;}
-/* Handle on hover */::-webkit-scrollbar-thumb:hover {background: #888;}
-</style>
 <em id="page-top"></em>
 <!--NAVIGATION-->
-<nav id="navbar-main" class="navbar navbar-dark bg-dark fixed-top p-0" style="">
-	<div class="container py-2 px-0 px-md-4">
+<nav id="navbar-main" class="navbar navbar-expand-lg">
+	<div class="container">
 	  <a class="navbar-brand col-8 ml-3" href="<?php echo Template::siteUrl() ?>">
 	  <span class="logo-main font-weight-bold"><?php echo Template::siteTitle() ?></span> <span class="logo-description"><?php echo Template::siteDescription() ?></span></a>
 	  </a>
-	  <button class="navbar-toggler text-primary mr-3" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-	  <span class="navbar-toggler-icon"></span>	
-	  </button>
-	  <div class="collapse navbar-collapse py-md-4 px-4" id="navbarsExampleDefault">
-		<?php echo Template::menu(2,8) ?>	
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMain" aria-controls="navbarMain" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+	  <div class="collapse navbar-collapse" id="navbarMain">
+		<?php echo Template::treeMenu(['children_only' => false]) ?>	
 	  </div>
   </div>
 </nav>
@@ -64,58 +50,12 @@ defined('SL_ROOT') or exit
 	</section>
 </main>
 <!--FOOTER-->
-<footer class="text-dark bg-white">
-	<div class="text-center px-3 py-4 social-links">
-		<a href="https://www.instagram.com/laszlobodnar.eu/" rel="nofollow" target="_blank" title="instagram" class="btn btn-primary">
-		<span class="icon-instagram"></span></a>
-		<a href="https://www.facebook.com/laszlobodnarphoto/" rel="nofollow" target="_blank"  title="facebook" class="btn btn-primary">
-		<span class="icon-facebook"></span></a>
-		<a href="https://www.linkedin.com/profile/preview?vpa=pub&locale=en_EN" rel="nofollow" target="_blank" title="linkedin" class="btn btn-primary"><span class="icon-linkedin2"></span></a>
-		<a href="https://pentaxphotogallery.com/laszlobodnar" rel="nofollow" target="_blank" title="pentaxphotogallery" class="btn btn-primary">
-		<span class="icon-camera"></span></a>
-		<a href="https://www.behance.net/ladislavbodnr" rel="nofollow" target="_blank" title="behance" class="btn btn-primary">
-		<span class="icon-behance"></span></a>	
-	</div>
-	<div class="text-center p-3">
-		<ul class="p-1">
-		<li class="p-0 col-12">&copy; 2020 - <a href="http://laszlobodnar.eu" rel="nofollow"><?php echo Template::siteTitle() ?></a> | <?php echo Template::siteDescription() ?> <br> All rights reserved. | Powered by <a href="https://sunlight-cms.org/" target="_blank" rel="nofollow">SunLight CMS</a></li>
-		<!--<?php echo Template::links() ?>-->
-		</ul>
+<footer id="footer-main" class="">
+	<div class="copyrights p-0 col-12">&copy; <?php echo date("Y"); ?> - <a href="<?php echo Template::siteUrl() ?>" rel="nofollow"><?php echo Template::siteTitle() ?></a> | <?php echo Template::siteDescription() ?> <br> All rights reserved. | Powered by <a href="https://sunlight-cms.org/" target="_blank" rel="nofollow">SunLight CMS</a></li>
+		<?php echo Template::links() ?>
 		<div class="user-menu-wrap text-center p-1"><?php echo Template::userMenu() ?></div>
-		<a href="#page-top"><span class="back-top btn btn-outline-primary p-2 icon-arrow-up2" style="display: none;"></span></a>
+		<a href="#page-top" class="back-top fixed-bottom" style="display: block;"><i class="bi bi-arrow-up-circle"></i></a>
     </button> 
 	</div>
 	<endora>
 </footer>
-<!-- Go to www.addthis.com/dashboard to customize your tools -->
-<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5e4c29817067f0a9"></script>
-<script>
-// When the user scrolls down 50px from the top of the document, resize the header's font size
-//Menu - scroll
-window.onscroll = function() {
-    if ( window.pageYOffset > 50 ) { 
-      $('#navbar-main').addClass('scroll');
-    }
-    else {     
-      $('#navbar-main').removeClass('scroll');
-    }
-	if ( window.pageYOffset > 100 ) {
-        $('.back-top').fadeIn();
-    } 
-	else {
-        $('.back-top').fadeOut();
-    }
-};
-  //ANIMATE ANCHOR
-	    $('a[href^="#"]').on('click', function(event) {
-        var target = $(this.getAttribute('href'));
-        if( target.length ) {
-            event.preventDefault();
-            $('html, body').stop().animate({
-                scrollTop: target.offset().top
-            }, 1000);
-        }
-    });
-	$('<li class="addthis_inline_share_toolbox"></li>').appendTo('.t-page .article-info');	
-</script>
-<script src="https://cmp.osano.com/Azyw3pRs9VlKv2U1s/93aeaadd-1fc3-4925-8592-49e41674a285/osano.js"></script>
