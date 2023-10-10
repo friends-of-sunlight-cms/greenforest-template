a71b275fe4de58c9cdd205059da26146d3f3ec46
Jirka Daněk <jirka.danek@jdanek.eu>
2023-10-10 10:00:15 +0200

    Use Router for bg files

--- plugins/templates/greenforest_v2/template.php
+++ plugins/templates/greenforest_v2/template.php
@@ -1,4 +1,6 @@
 <?php
+
+use Sunlight\Router;
 use Sunlight\Template;
 defined('SL_ROOT') or exit
 ?>
@@ -13,13 +15,13 @@ defined('SL_ROOT') or exit
       <span class="navbar-toggler-icon"></span>
     </button>
 	  <div class="collapse navbar-collapse" id="navbarMain">
-		<?php echo Template::treeMenu(['children_only' => false]) ?>	
+		<?php echo Template::treeMenu(['children_only' => false]) ?>
 	  </div>
   </div>
 </nav>
 <!--MAIN-->
 <main role="main" class="pt-5">
-	<section class="col-12 pt-5 portfolio-select"style="background-image: url('/upload/bg.jpg');background-size:cover;background-attachment: fixed;">
+	<section class="col-12 pt-5 portfolio-select"style="background-image: url('<?= Router::path('upload/bg.jpg') ?>');background-size:cover;background-attachment: fixed;">
 		<div class="container py-3 py-lg-5 px-0 px-md-5">
 			<div class="row m-0 p-0">
 				<div class="col-12 p-3 m-0">
@@ -30,7 +32,7 @@ defined('SL_ROOT') or exit
 		<a href="#portfolio-cat-select" class="next text-center">Continue<br><span class="btn mt-4 text-white icon-arrow-down2"></span></a>
 	</section>
 	<em id="portfolio-cat-select"></em>
-	<section class="col-12 content bg-white" >		
+	<section class="col-12 content bg-white" >
 		<div class="container py-lg-5 px-0 px-md-5">
 			<div class="row m-0 p-0">
 				<div class="col-12 p-0 m-0">
@@ -39,7 +41,7 @@ defined('SL_ROOT') or exit
 			</div>
 		</div>
 	</section>
-	<section class="col-12 box-content" style="background-image: url('/upload/bg.jpg');background-size:cover;background-attachment: fixed;">
+	<section class="col-12 box-content" style="background-image: url('<?= Router::path('upload/bg.jpg') ?>');background-size:cover;background-attachment: fixed;">
 		<div class="container py-5 px-0 px-md-5 mt-2">
 			<div class="row m-0 p-0">
 				<div class="col-12 p-0 m-0">
@@ -55,7 +57,7 @@ defined('SL_ROOT') or exit
 		<?php echo Template::links() ?>
 		<div class="user-menu-wrap text-center p-1"><?php echo Template::userMenu() ?></div>
 		<a href="#page-top" class="back-top fixed-bottom" style="display: block;"><i class="bi bi-arrow-up-circle"></i></a>
-    </button> 
+    </button>
 	</div>
 	<endora>
 </footer>
